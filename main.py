5064e3cdbb96a9fd2d5464c1a7983beb56bdcc6a
ubaid ul majied <52852783+Ubaidb936@users.noreply.github.com>
2024-04-27 20:23:53 -0700

    upload pdf to vector db

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI, UploadFile, File
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+from langchain_chroma import Chroma
 
 # Create an instance of the FastAPI class
 app = FastAPI()
+
+# create the open-source embedding function
+embedding_function = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2")
 
 
 @app.post("/upload")
@@ -14,7 +21,7 @@ async def upload_file(file: UploadFile = File(...)):
     
     loader = PyPDFLoader(file.filename)
     
-    ##splitting the text file
+    #splitting the text file
     text_splitter = RecursiveCharacterTextSplitter(
             chunk_size=600,  
             chunk_overlap=60,
@@ -22,9 +29,10 @@ async def upload_file(file: UploadFile = File(...)):
             separators=["\n\n", "\n", ".", " ", ""],
         )
     
-    langchain_docs = loader.load_and_split(text_splitter=text_splitter) 
+    docs = loader.load_and_split(text_splitter=text_splitter) 
     
+    db = Chroma.from_documents(docs, embedding_function, persist_directory="./chroma_db")
     
-        
-        
-    return {"contents":  langchain_docs[0]}
+    docs = db.similarity_search("Who is Ubaid")    
+    
+    return {"contents":  docs[0].page_content}
